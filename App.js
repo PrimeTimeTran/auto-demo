23b49fd593c463c0b4ba3b0d7d5d5b6a97bbc9c1
Loi Tran <primetimetran@gmail.com>
2023-04-19 17:13:58 -0400

    Remove dumb files

--- App.js
+++ App.js
@@ -13,8 +13,9 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    height: 100,
+    width: 100,
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
   },
